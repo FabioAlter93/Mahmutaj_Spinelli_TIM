071f2e77dbeb1430291ca093d8a574d3ab055fda
FabioAlter93 <spacino12@hotmail.it>
2016-07-16 17:58:14 +0200

    includes php

---
+++ website/includes/php/getAssistenza.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+define('MYSQL_BOTH',MYSQLI_BOTH);
+define('MYSQL_NUM',MYSQLI_NUM);
+define('MYSQL_ASSOC',MYSQLI_ASSOC);
+
+$id = intval($_GET['id']);
+
+$mysqli = new mysqli("localhost", "root", "", "myTimDB");
+
+if (mysqli_connect_errno()) { 
+    echo "Error to connect to DBMS: ".mysqli_connect_error();
+    exit();
+}
+else {
+    $query ="SELECT * FROM serviziAssistenza WHERE serviziAssistenza.id = '".$id."'";
+
+    $result = $mysqli->query($query);
+
+    if($result->num_rows >0)
+    {
+        $row = $result->fetch_array(MYSQL_ASSOC);
+        $myObject = array_map('utf8_encode', $row);
+     
+        echo json_encode($myObject);
+    }
+
+    $result->close();
+
+    $mysqli->close();
+}
